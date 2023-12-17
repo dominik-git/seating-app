18a4211b301df71a9e829d678a65cd20a53418c0
Dominik Kolesar <dominik.kolesar@visma.com>
2023-12-17 17:05:41 +0100

    fix material

--- src/app/modules/edit-places/modals/assign-fixed-place-dialog.ts
+++ src/app/modules/edit-places/modals/assign-fixed-place-dialog.ts
@@ -1,12 +1,12 @@
 import { Component, Inject, OnInit } from '@angular/core';
-import { MAT_LEGACY_DIALOG_DATA as MAT_DIALOG_DATA, MatLegacyDialogRef as MatDialogRef } from '@angular/material/legacy-dialog';
 
 import { BookingResourceService } from '../../../services/booking/booking-resource.service';
 import { SeatsInRange } from '../../../models/booking.model';
-import {FixedPlaceModel} from "../../../models/fixedPlace.model";
-import {UntypedFormControl} from "@angular/forms";
-import {Observable} from "rxjs";
-import {map, startWith} from "rxjs/operators";
+import { FixedPlaceModel } from '../../../models/fixedPlace.model';
+import { UntypedFormControl } from '@angular/forms';
+import { Observable } from 'rxjs';
+import { map, startWith } from 'rxjs/operators';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 
 @Component({
   selector: 'assign-fixed-place-dialog',
@@ -18,23 +18,26 @@ export class AssignFixedPlaceDialog implements OnInit {
   bookedDays: Date[] = [];
   loading = false;
   myControl = new UntypedFormControl();
-  options = [{fullName: 'Mary', userId:1, position:'software engineer'}, {fullName: 'Shelley', userId:2,position:'software engineer'}, {fullName: 'Igor', userId:3,position:'software engineer'}];
+  options = [
+    { fullName: 'Mary', userId: 1, position: 'software engineer' },
+    { fullName: 'Shelley', userId: 2, position: 'software engineer' },
+    { fullName: 'Igor', userId: 3, position: 'software engineer' },
+  ];
   filteredOptions: Observable<any[]>;
 
   constructor(
     public dialogRef: MatDialogRef<AssignFixedPlaceDialog>,
     @Inject(MAT_DIALOG_DATA)
-    public data: { svgElement:any, fixedPlace:FixedPlaceModel },
+    public data: { svgElement: any; fixedPlace: FixedPlaceModel },
     private bookingResourceService: BookingResourceService
   ) {}
 
   ngOnInit() {
     this.filteredOptions = this.myControl.valueChanges.pipe(
       startWith(''),
-      map(value => (typeof value === 'string' ? value : value.name)),
-      map(name => (name ? this._filter(name) : this.options.slice())),
+      map((value) => (typeof value === 'string' ? value : value.name)),
+      map((name) => (name ? this._filter(name) : this.options.slice()))
     );
-
   }
 
   displayFn(user: any): string {
@@ -44,23 +47,32 @@ export class AssignFixedPlaceDialog implements OnInit {
   private _filter(name: string): any[] {
     const filterValue = name.toLowerCase();
 
-    return this.options.filter(option => option.fullName.toLowerCase().includes(filterValue));
+    return this.options.filter((option) =>
+      option.fullName.toLowerCase().includes(filterValue)
+    );
   }
 
   assignPlace() {
     const placeId = this.data.svgElement.getAttribute('id').toString();
-    const fixedPlace = {...this.myControl.value, placeId}
+    const fixedPlace = { ...this.myControl.value, placeId };
     // const fixedPlaceMock = {
     //     placeId: svgElementId,
     //     fullName: 'Roman Klimcik',
     //     position: 'software engineer',
     //     userId: 1
     // }
-    this.dialogRef.close({ assigned: true,svgElement : this.data.svgElement, fixedPlace:fixedPlace});
+    this.dialogRef.close({
+      assigned: true,
+      svgElement: this.data.svgElement,
+      fixedPlace: fixedPlace,
+    });
   }
 
   unAssignPlace() {
-    this.dialogRef.close({ assigned: false ,svgElement : this.data.svgElement, fixedPlace:this.data.fixedPlace});
+    this.dialogRef.close({
+      assigned: false,
+      svgElement: this.data.svgElement,
+      fixedPlace: this.data.fixedPlace,
+    });
   }
-
 }
